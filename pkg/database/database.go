45707e82618544ceae075008f675968e8140e857
777Matvei777 <matveidurin@gmail.com>
2024-04-07 19:19:15 +0300

    Убраны комментарии

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -23,11 +23,6 @@ func CreateDataBase(data map[int]interface{}, Db_path string) {
 }
 
 func WriteData(n int, Db_path string) {
-	// f, _ := os.Open(Db_path)
-	// defer f.Close()
-	// jsondata, _ := os.ReadFile(Db_path)
-	// var items map[int]Item
-	// _ = json.Unmarshal(jsondata, &items)
 	if n > 0 {
 		for i := 1; i < n+1; i++ {
 			fmt.Println(items[i])
