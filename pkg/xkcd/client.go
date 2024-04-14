95d050c8bae387b7b88a9be0d8ac04747fa3d2e6
777Matvei777 <matveidurin@gmail.com>
2024-04-14 22:55:49 +0300

    Убраны лишние комментарии

--- pkg/xkcd/client.go
+++ pkg/xkcd/client.go
@@ -21,7 +21,6 @@ func Parse(Url string, Parallel int, ctx context.Context, num int, exist map[int
 	var wg sync.WaitGroup
 	var mutex sync.Mutex
 	ch := make(chan int, Parallel)
-	//found404 := make(chan bool)
 	flag := false
 	for i := num; !flag; i++ {
 		select {
@@ -60,38 +59,6 @@ func Parse(Url string, Parallel int, ctx context.Context, num int, exist map[int
 					if i%100 == 0 {
 						fmt.Printf("Загружен %d-ый комикс\n", i)
 					}
-					// select {
-					// case <-found404:
-					// 	return
-					// default:
-					// 	defer func() { <-ch }()
-
-					// 	address := fmt.Sprintf("%s/%d/info.0.json", Url, i)
-					// 	resp, err := http.Get(address)
-					// 	if err != nil {
-					// 		fmt.Println("getting error:", err)
-					// 		return
-					// 	}
-					// 	defer resp.Body.Close()
-					// 	if resp.StatusCode == 404 && i != 404 {
-					// 		flag = true
-					// 		found404 <- true
-					// 		return
-					// 	}
-					// 	var oneData XkcdStruct
-					// 	data, err := io.ReadAll(resp.Body)
-					// 	if err != nil {
-					// 		fmt.Println("reading error:", err)
-					// 		return
-					// 	}
-					// 	_ = json.Unmarshal([]byte(data), &oneData)
-					// 	mutex.Lock()
-					// 	Db = append(Db, oneData)
-					// 	mutex.Unlock()
-					// 	if i%100 == 0 {
-					// 		fmt.Printf("Загружен %d-ый комикс\n", i)
-					// 	}
-					// }
 
 				}(i)
 			}
