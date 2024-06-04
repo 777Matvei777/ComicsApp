7c249468b95c310c40870d046ae24950ddb38969
777Matvei777 <matveidurin@gmail.com>
2024-06-04 21:04:58 +0300

    fix e2e test

--- e2e_test.sh
+++ e2e_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-container_id=$(docker run -d -e POSTGRES_PASSWORD=local -p 5432:5432 8e4fc9e18489)
+docker run -d -e POSTGRES_PASSWORD=local -p 5432:5432 --name mytestpostgres postgres
 
 sleep 5
 
@@ -10,20 +10,19 @@ sleep 5
 jwt_token=$(curl -s -X POST -H "Content-Type: application/json" -d '{"username": "Matvei", "password": "1234"}' http://localhost:8080/login)
 if [ -z "$jwt_token" ]; then
     echo "Не удалось получить токен"
-    docker stop $container_id
-    docker rm $container_id
+    docker rm -f mytestpostgres
     exit 1
 fi
 
 curl -s -X POST -H "Authorization: $jwt_token" http://localhost:8080/update
 sleep 2
-curl -s http://localhost:8080/pics?search="apple,doctor"
-# if echo "$response" | grep -q "apple a day"; then
-#     echo "Тест пройден"
-# else
-#     echo "Тест не пройден"
-# fi
+response=$(curl -s http://localhost:8080/pics?search="apple,doctor,Granny")
+echo "$response"
+if echo "$response" | grep -q "https://imgs.xkcd.com/comics/an_apple_a_day.png"; then
+    echo "Тест пройден"
+else
+    echo "Тест не пройден"
+fi
 
 kill %1
-docker stop $container_id
-docker rm $container_id
+docker rm -f mytestpostgres
